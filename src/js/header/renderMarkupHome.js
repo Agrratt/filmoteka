872a7b7026515874a29af2f07e2a17d2b3dbc237
aleksandr105 <aleksandrsherbina105@gmail.com>
2022-02-01 22:33:02 +0100

    Add return to home

--- src/js/header/renderMarkupHome.js
+++ src/js/header/renderMarkupHome.js
@@ -1,25 +1,36 @@
+import fetchFavoritesMovies from '../api/fetchFavoritesMovies';
+import preloader from '../main/preloader';
+import { arrayGenres, renderGallery } from '../main/renderMain';
+
 const refs = {
-    homeLink: document.querySelector('.page__home'),
-    libraryLink: document.querySelector('.page__library'),
-    headerContainer: document.querySelector('.header'),
-    libraryButtonBox: document.querySelector('.library__button-box'),
-    formSearch: document.querySelector('.form__search')
+  gallery: document.querySelector('.list_film'),
+  homeLink: document.querySelector('.page__home'),
+  libraryLink: document.querySelector('.page__library'),
+  headerContainer: document.querySelector('.header'),
+  libraryButtonBox: document.querySelector('.library__button-box'),
+  formSearch: document.querySelector('.form__search'),
 };
 
 refs.homeLink.addEventListener('click', renderMarkupHome);
 
 export default function renderMarkupHome(e) {
-    e.preventDefault();
-    if (!refs.homeLink.classList.contains('active')) {
-        
-        refs.headerContainer.classList.add('header')
-        refs.headerContainer.classList.remove('header__library');
+  e.preventDefault();
 
-        refs.libraryLink.classList.remove('active');
-        refs.homeLink.classList.add('active');
+  fetchFavoritesMovies().then(data => {
+    preloader();
 
-        refs.libraryButtonBox.classList.add('is-hidden');
-        refs.formSearch.classList.remove('is-hidden');
-    }
+    refs.gallery.innerHTML = '';
+    refs.gallery.insertAdjacentHTML('beforeend', renderGallery(data.results));
+  });
+
+  if (!refs.homeLink.classList.contains('active')) {
+    refs.headerContainer.classList.add('header');
+    refs.headerContainer.classList.remove('header__library');
+
+    refs.libraryLink.classList.remove('active');
+    refs.homeLink.classList.add('active');
+
+    refs.libraryButtonBox.classList.add('is-hidden');
+    refs.formSearch.classList.remove('is-hidden');
+  }
 }
-
