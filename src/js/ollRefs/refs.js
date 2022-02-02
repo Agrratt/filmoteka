5e72c678f87f2fdd98803644b05674d7cbe3dde4
Kateryna Tolstykh <agrratt@gmail.com>
2022-02-02 20:56:41 +0200

    правки в ховере, перенос рефов

--- src/js/ollRefs/refs.js
+++ src/js/ollRefs/refs.js
@@ -2,6 +2,8 @@ const refs = {
   gallery: document.querySelector('.list_film'),
   library: document.querySelector('.page__library'),
   home: document.querySelector('.page__home'),
+  headerContainer: document.querySelector('.header'),
+  libraryButtonBox: document.querySelector('.library__button-box'), 
   navigate: document.querySelector('.navigate__list'),
   searchForm: document.querySelector('.form__search'),
   searchHelper: document.querySelector('.search-helper'),
