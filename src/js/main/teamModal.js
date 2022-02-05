6f4cd7498d796f8f10f3a84fe2f43c4ccce193a6
Ruslan Sulieimanov <ruslansulieim@gmail.com>
2022-02-05 17:46:55 +0200

    add click
    добавив на модалку виключити по ESC та backdrop

--- src/js/main/teamModal.js
+++ src/js/main/teamModal.js
@@ -11,6 +11,7 @@ window.onload = externalLinks;
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
+    backdropClick: document.querySelector('.mod__team'),
     modal: document.querySelector('[data-modal]'),
   };
 
@@ -19,5 +20,24 @@ window.onload = externalLinks;
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+    window.addEventListener('keydown', onEscClick);
+  }
+
+  refs.backdropClick.addEventListener('click', onBackdropClick);
+  document.querySelector('body').style.overflow = 'auto';
+
+  function onBackdropClick(event) {
+    if (event.currentTarget === event.target) {
+      toggleModal();
+    }
+  }
+
+  function onEscClick(event) {
+    const ESC_KEY_CODE = 'Escape';
+    console.log(event.code);
+
+    if (event.code === ESC_KEY_CODE) {
+      toggleModal();
+    }
   }
 })();
